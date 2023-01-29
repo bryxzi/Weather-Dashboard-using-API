81d6cba9c2867d256543b424a9a9e71be16228ee
bryxzi <122295924+bryxzi@users.noreply.github.com>
2023-01-29 13:35:28 -0500

    added starting files

---
+++ develop/script.js
@@ -0,0 +1,13 @@
+
+
+var userInput = document.getElementById('city');
+var searchBtn = document.getElementById('submitBtn');
+
+searchBtn.addEventListener('click', function(){
+
+    console.log(userInput);
+
+
+}); 
+
+
